136f3fc4f16929ff805c3e9fb2e7346a267c87ab
sankettgorey <sankettgorey@gmail.com>
2023-02-19 22:45:44 +0530

    notes

--- Books_Project/app.py
+++ Books_Project/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 import uvicorn
 
 app = FastAPI()
@@ -9,7 +9,7 @@ BOOKS = {'book1': {'title': 'title1', 'author': 'author1'},
          'book3': {'title': 'title3', 'author': 'author3'},
          'book4': {'title': 'title4', 'author': 'author4'},
          'book5': {'title': 'title5', 'author': 'author5'},
-        }
+         }
 
 
 @app.get('/')
@@ -22,11 +22,15 @@ async def books():
     return BOOKS
 
 # path parater
+
+
 @app.get('/param/{bookTitle}')
 async def pathParam(bookTitle: str):
     return {'bookTitle': bookTitle}
 
 # path paramter as integer
+
+
 @app.get('/intParam/{bookTitle}')
 async def intParam(bookTitle: int):
     return {'bookTitle': bookTitle}
